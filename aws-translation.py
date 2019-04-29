f42df342d707ed4a657429a8da7b279f2864600e
bhushan <bhushan.s.94@gmail.com>
2019-04-29 10:48:03 -0700

    AWS Translation

--- aws-translation.py
+++ aws-translation.py
@@ -1,7 +1,20 @@
 import boto3
 
-translate = boto3.client(service_name='translate', region_name='us-west-2', use_ssl=True)
+# TODO:
+# Set regision_name
+# Set Access keys in aws configuration
+# Pass Src and Dest language pair from
+# Following supported lanauge pair
+# https://docs.aws.amazon.com/translate/latest/dg/pairs.html
+def translate(input_txt, src_lang, dst_lang):
+  translate = boto3.client(service_name='translate', region_name='us-west-2', use_ssl=True)
 
-result = translate.translate_text(Text="Hello, World", 
-            SourceLanguageCode="en", TargetLanguageCode="de")
-print('TranslatedText: ' + result.get('TranslatedText'))
+  result = translate.translate_text(Text=input_txt, 
+              SourceLanguageCode=src_lang, TargetLanguageCode=dst_lang)
+        
+  # print('TranslatedText: ' + result.get('TranslatedText'))
+  return result.get('TranslatedText')
+
+# Test translation API below
+# print(translate("Hello World! Testing translation", 'en', 'de'))
+
